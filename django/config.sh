d9033dda721ba966cb100f94cef249bfb37d192a
Chris Chambers <magma.chambers@gmail.com>
2014-02-28 01:53:06 +0000

    Add django config

---
+++ django/config.sh
@@ -0,0 +1,1 @@
+source_platform_specific_file_for "$DOTFILES/django/config.sh"
--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,11 +1,3 @@
-# If you don't have the beep utility installed, create a simple system beep
-# alias. This may or may not emit a chime depending on the OS (it doesn't do
-# anything for cygwin on Windows 7, for example):
-
-if [[ -z $(which beep > /dev/null 2>&1) ]]; then
-    alias beep='printf "\a"'
-fi
-
 alias h='history | tail -n 10'    # previous 10 history lines, for context
 
 if ls --color -d . >/dev/null 2>&1; then
@@ -19,3 +11,5 @@ elif ls -G -d . >/dev/null 2>&1; then
 fi
 
 alias tree='tree -FC'             # colorise and use symbolic key for filetypes
+
+source_platform_specific_file_for "$DOTFILES/shell/aliases.sh"
