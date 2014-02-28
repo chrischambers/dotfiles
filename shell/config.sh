a4b1e24336ea4ba5a31d990498a028fa183c28f6
Chris Chambers <magma.chambers@gmail.com>
2014-02-28 02:00:15 +0000

    Add shared config and aliases for zsh/bash

---
+++ shell/config.sh
@@ -0,0 +1,18 @@
+# Common Configuration:
+# ---------------------
+
+export MANPATH="$HOME/man:$MANPATH"
+# ...should contain man1/plod.1 to find manfile for plod, for example.
+
+# Useful Variables: {{{
+# ----------------------------------------------------------------------------
+# export TERM=xterm-color
+# export COLORTERM=xterm
+# export CLICOLOR=true
+export PAGER=less
+export LESS=-r    # Outputs raw control characters: necessary if you're piping
+                  # in colourised output (as ipython does)
+# ----------------------------------------------------------------------------
+# }}}
+
+source_platform_specific_file_for "$DOTFILES/shell/config.sh"
