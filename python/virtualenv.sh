2b291b211977da06b8bf1da9531051e24c0a0e61
Chris Chambers <magma.chambers@gmail.com>
2014-02-28 01:53:37 +0000

    Add python config

---
+++ python/virtualenv.sh
@@ -0,0 +1,16 @@
+# Virtualenv and Pip Configuration: {{{
+# ----------------------------------------------------------------------------
+export WORKON_HOME="$HOME/.virtualenvs"
+if [[ -r $virtualenvwrapper_loc ]]; then
+    source $virtualenvwrapper_loc
+fi
+# Note: switching envs seems to undo the PATH manipulation above.
+
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
+# makes pip detect an active virtualenv and install to it, without having to
+# pass it the -E parameter
+# Source: <http://www.doughellmann.com/docs/virtualenvwrapper/tips.html>
+export PIP_RESPECT_VIRTUALENV=true
+
+# }}}
+# ----------------------------------------------------------------------------
