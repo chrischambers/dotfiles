dc5c94a5d04e51d45d3c20d15981f178497b6569
Chris Chambers <magma.chambers@gmail.com>
2014-03-05 17:04:18 +0000

    Saner macvim defaults, through installation via macports

--- vim/config.sh
+++ vim/config.sh
@@ -3,9 +3,9 @@
 export EDITOR=vim
 export VISUAL="vim -g"
 export GIT_EDITOR="vim"
+alias vi="vim"
 alias gvim="vim -g"
 alias g='gvim --servername Vim1 --remote-silent'
 alias vimprof='vim --startuptime /dev/stdout -c "qa" | grep -v "^[^0-9].*" | sort'
 
 source_platform_specific_file_for "$DOTFILES/vim/config.sh"
-
