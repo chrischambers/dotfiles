b308a3ee5e42611c7f43d17157edc87e3b4d5ec2
Chris Chambers <magma.chambers@gmail.com>
2015-08-04 14:20:02 +0100

    Part-complete: vim execution fixes.
    Fixing vim so that:
    * git-editor works successfully
    * visual line edit in shell (i.e. switch to vim mode, press v) works
    * all files opened in gvim should open in single gui session

--- vim/config.sh
+++ vim/config.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-export EDITOR=vim
-export VISUAL="vim -g"
-export GIT_EDITOR="vim"
-alias vi="vim"
-alias gvim="vim -g"
-alias g='gvim --servername Vim1 --remote-silent'
-alias vimprof='vim --startuptime /dev/stdout -c "qa" | grep -v "^[^0-9].*" | sort'
+# Defaults:
+vim=vim
+gvim=gvim
 
 source_platform_specific_file_for "vim/config.sh"
+
+alias vi=$vim
+
+function visvim () { $vim $1 }
+export EDITOR=$vim
+export VISUAL=visvim
+export GIT_EDITOR=$vim
+
+# Vim Profiler:
+alias vimprof="vim --startuptime /dev/stdout -c \"qa\" | grep -v \"^[^0-9].*\" | sort"
+
+function g () {
+    # Should ensure that only one GUI is created and that subsequently opened
+    # files will be opened within that one GUI.
+    $gvim --nofork --servername Vim1 --remote-silent "$@" &
+}
