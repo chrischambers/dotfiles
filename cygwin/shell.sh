92441ef21df2b5a00b8909f453d7396491f3ff34
Chris Chambers <magma.chambers@gmail.com>
2017-03-31 23:06:47 +0100

    "uname -o" not valid on OSX

--- cygwin/shell.sh
+++ cygwin/shell.sh
@@ -1,6 +1,7 @@
-if [[ $(uname -o) == "Cygwin" ]]; then
-    # Overrides virtualenv pip switching:
-    # alias pip=pip3
-    export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-    source /usr/bin/virtualenvwrapper.sh
-fi
+# -o flag isn't valid on OSX
+# if [[ $(uname -o) == "Cygwin" ]]; then
+#     # Overrides virtualenv pip switching:
+#     # alias pip=pip3
+#     export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+#     source /usr/bin/virtualenvwrapper.sh
+# fi
